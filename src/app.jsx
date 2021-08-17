b9126b0c502436d422b176831bf6b0b1c0c78451
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-08-18 02:15:48 +0900

    Firebase Authentication을 통한 login page 구현 및 UI 디자인 적용

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,12 @@
-
-import './app.css';
+import styles from './app.module.css';
 import Login from './components/login/login';
 
-
-function App({authService}) {
+function App({ authService }) {
   return (
-    <>
-      <Login authService={authService}/>
-    </>
-  )
+    <div className={styles.app}>
+      <Login authService={authService} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,12 @@
-
 import React from 'react';
+import styles from './footer.module.css';
 
 const Footer = (props) => (
-      <h1>Footer</h1>
-  );
+  <footer className={styles.footer}>
+    <p className={styles.title}>
+      Please Login with your Google or Github account.
+    </p>
+  </footer>
+);
 
 export default Footer;
