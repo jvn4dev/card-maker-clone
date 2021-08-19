322b15dfe058f061df8a9d8b76fb7564c0eaf26e
Chung Seong Jun <77430059+jvn4dev@users.noreply.github.com>
2021-08-19 18:22:31 +0900

    card 컴포넌트 추가 및 css 적용

---
+++ src/components/card/card.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './card.module.css';
+
+const Card = ({ card }) => {
+  const { name, company, title, email, message, theme, fileName, fileURL } =
+    card;
+  return (
+    <li className={`${styles.card} ${getTheme(theme)}`}>
+      <img className={styles.image} src={fileURL} alt="profile image" />
+      <div className={styles.container}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.company}>{company}</p>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.email}>{email}</p>
+        <p className={styles.message}>{message}</p>
+      </div>
+    </li>
+  );
+};
+
+function getTheme(theme) {
+  switch (theme) {
+    case 'dark':
+      return styles.dark;
+    case 'light':
+      return styles.light;
+    case 'colorful':
+      return styles.colorful;
+    default:
+      throw new Error(`unknown theme: ${theme}`);
+  }
+}
+
+export default Card;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -8,6 +8,43 @@ import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
   const history = useHistory();
+
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      name: 'Chung',
+      company: 'Zepeto',
+      title: 'Front-end Developer',
+      email: 'jvn4develop@gmail.com',
+      message: "Don't be a code Monkey",
+      theme: 'dark',
+      fileName: 'chung.png',
+      fileURL: './images/default_logo.png',
+    },
+    {
+      id: 2,
+      name: 'Kwon',
+      company: 'LH',
+      title: 'daeri',
+      email: 'jvn4develop@gmail.com',
+      message: "Don't be a code Monkey",
+      theme: 'light',
+      fileName: 'chung.png',
+      fileURL: './images/default_logo.png',
+    },
+    {
+      id: 3,
+      name: 'MR.NOBODY',
+      company: 'Zepeto',
+      title: 'Front-end Developer',
+      email: 'jvn4develop@gmail.com',
+      message: "Don't be a code Monkey",
+      theme: 'colorful',
+      fileName: 'chung.png',
+      fileURL: './images/default_logo.png',
+    },
+  ]);
+
   const onLogout = () => {
     authService.logout();
   };
@@ -24,8 +61,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
